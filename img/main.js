a1f05e4b02c8b332553bd2491e8cf7a4f50f3038
Fabo xw <fabianmartinezrincon.123@gmail.com>
2021-11-25 03:13:01 -0300

    pages

--- img/main.js
+++ img/main.js
@@ -1,6 +1,8 @@
+import './style.css';
+
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import './style.css'
+
 // Setup
 
 const scene = new THREE.Scene();
